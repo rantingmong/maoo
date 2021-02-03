41f131e94bee71d035588365843ae33703540d2b
Michael Ong <michaelong18@outlook.com>
2021-02-04 00:43:05 +0800

    Removing all code~

--- app/src/main/java/net/rantingmong/maoo/ui/sample/holder/FeedViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package net.rantingmong.maoo.ui.sample.holder
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class FeedViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-}
--- backend/src/main/java/net/rantingmong/maoo/shared/viewmodel/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package net.rantingmong.maoo.shared.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    var title = "Henlo there~"
-}
